c1af6f8aae2c14baa76201613a17c58bc79e0422
longpi1 <longping1999@163.com>
2022-08-03 22:43:42 +0800

    添加设计模式

---
+++ 00_singleton/README.md
@@ -0,0 +1,4 @@
+# 单例模式
+
+lazySingleton: 使用懒惰模式的单例模式，使用双重检查加锁保证线程安全
+hungrySingleton: 使用饿汉式的单例模式
--- 01_simple_factory/simple.go
+++ 01_simple_factory/simple.go
@@ -2,12 +2,12 @@ package simplefactory
 
 import "fmt"
 
-//API is interface
+//接口
 type API interface {
 	Say(name string) string
 }
 
-//NewAPI return Api instance by type
+//根据type返回不同的结构体
 func NewAPI(t int) API {
 	if t == 1 {
 		return &hiAPI{}
@@ -17,18 +17,18 @@ func NewAPI(t int) API {
 	return nil
 }
 
-//hiAPI is one of API implement
+//hiAPI
 type hiAPI struct{}
 
-//Say hi to name
+
 func (*hiAPI) Say(name string) string {
 	return fmt.Sprintf("Hi, %s", name)
 }
 
-//HelloAPI is another API implement
+//HelloAPI
 type helloAPI struct{}
 
-//Say hello to name
+
 func (*helloAPI) Say(name string) string {
 	return fmt.Sprintf("Hello, %s", name)
 }
